ee736837b9000218ba2e16be5d254ef6df759217
CodeNinjasEtobicoke <148917437+iam9116@users.noreply.github.com>
2024-01-09 17:57:55 -0500

    Toilet
    I added a giant toilet

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public string triggerName = "";
+
+    public GameObject breadPrefab;
+
+    public GameObject heldItem;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (triggerName == "Bread")
+            {
+                heldItem = Instantiate(breadPrefab, transform, false);
+                heldItem.transform.localPosition = new Vector3(0f, 2f, 1f);
+            }
+
+            if (triggerName == "Stove")
+            {
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerName = other.name;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerName = "";
+    }
+}
